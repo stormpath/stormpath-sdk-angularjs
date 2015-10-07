319412d1c098f2968c527ff179e9558938677872
Robert <robert@stormpath.com>
2015-10-07 09:15:52 -0700

    updates to the example application

--- example/dashboard-app/server/routes.js
+++ example/dashboard-app/server/routes.js
@@ -10,7 +10,7 @@ var ExpressStormpath = require('express-stormpath');
 module.exports = function(app) {
 
   // Insert routes below
-  app.use('/api/things', ExpressStormpath.apiAuthenticationRequired, require('./api/thing'));
+  app.use('/api/things', ExpressStormpath.loginRequired, require('./api/thing'));
 
   // All undefined asset or api routes should return a 404
   app.route('/:url(api|auth|components|app|bower_components|assets)/*')

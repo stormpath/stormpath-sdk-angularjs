cd8ef958d5a97a785599785c4b678cd0dd474fe6
Luka Skukan <luka.skukan@infinum.hr>
2016-12-08 13:30:16 +0100

    Improve svg handling and add hover state

--- src/stormpath.multifactor.js
+++ src/stormpath.multifactor.js
@@ -75,13 +75,9 @@ function FactorViewModel(factor) {
 }
 
 FactorViewModel.prototype.getIcon = function getIcon() {
-  var dataUrl = (
-    this.factor.type === 'sms'
-      ? '<svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path fill="#0072DD" d="M15.5 1h-8C6.12 1 5 2.12 5 3.5v17C5 21.88 6.12 23 7.5 23h8c1.38 0 2.5-1.12 2.5-2.5v-17C18 2.12 16.88 1 15.5 1zm-4 21c-.83 0-1.5-.67-1.5-1.5s.67-1.5 1.5-1.5 1.5.67 1.5 1.5-.67 1.5-1.5 1.5zm4.5-4H7V4h9v14z"/><path d="M0 0h24v24H0z" fill="none"/></svg>'
-      : '<svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path fill="#0072DD" d="M20 2H4c-1.1 0-1.99.9-1.99 2L2 22l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-2 12H6v-2h12v2zm0-3H6V9h12v2zm0-3H6V6h12v2z"/><path d="M0 0h24v24H0z" fill="none"/></svg>'
-  );
-
-  return 'data:image/svg+xml,' + dataUrl;
+  return this.factor.type === 'sms'
+    ? '<svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path class="path" d="M15.5 1h-8C6.12 1 5 2.12 5 3.5v17C5 21.88 6.12 23 7.5 23h8c1.38 0 2.5-1.12 2.5-2.5v-17C18 2.12 16.88 1 15.5 1zm-4 21c-.83 0-1.5-.67-1.5-1.5s.67-1.5 1.5-1.5 1.5.67 1.5 1.5-.67 1.5-1.5 1.5zm4.5-4H7V4h9v14z"/><path d="M0 0h24v24H0z" fill="none"/></svg>'
+    : '<svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path class="path" d="M20 2H4c-1.1 0-1.99.9-1.99 2L2 22l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-2 12H6v-2h12v2zm0-3H6V9h12v2zm0-3H6V6h12v2z"/><path d="M0 0h24v24H0z" fill="none"/></svg>';
 };
 
 FactorViewModel.prototype.getTitle = function getTitle() {
@@ -189,10 +185,14 @@ angular.module('stormpath.mfa', ['stormpath.CONFIG', 'stormpath.utils'])
   };
 }])
 
-.controller('spMultifactorSelectFormCtrl', ['StormpathMultifactorAuthenticator', 'FactorViewModel', '$scope', function(StormpathMultifactorAuthenticator, FactorViewModel, $scope) {
+.controller('spMultifactorSelectFormCtrl', ['StormpathMultifactorAuthenticator', 'FactorViewModel', '$scope', '$sce', function(StormpathMultifactorAuthenticator, FactorViewModel, $scope, $sce) {
   $scope.factorViewModels = $scope.factors.map(function(factor) {
     return new FactorViewModel(factor);
   });
+
+  $scope.clean = function clean(html) {
+    return $sce.trustAsHtml(html);
+  };
 
   $scope.select = function select(factor) {
     $scope.posting = true;

0edf1d8fa18ad0e443b9c1712f4b919e1c63901e
Luka Skukan <luka.skukan@infinum.hr>
2016-12-08 13:08:54 +0100

    Extract and style factor select form

--- src/stormpath.multifactor.js
+++ src/stormpath.multifactor.js
@@ -77,8 +77,8 @@ function FactorViewModel(factor) {
 FactorViewModel.prototype.getIcon = function getIcon() {
   var dataUrl = (
     this.factor.type === 'sms'
-      ? '<svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M15.5 1h-8C6.12 1 5 2.12 5 3.5v17C5 21.88 6.12 23 7.5 23h8c1.38 0 2.5-1.12 2.5-2.5v-17C18 2.12 16.88 1 15.5 1zm-4 21c-.83 0-1.5-.67-1.5-1.5s.67-1.5 1.5-1.5 1.5.67 1.5 1.5-.67 1.5-1.5 1.5zm4.5-4H7V4h9v14z"/><path d="M0 0h24v24H0z" fill="none"/></svg>'
-      : '<svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M20 2H4c-1.1 0-1.99.9-1.99 2L2 22l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-2 12H6v-2h12v2zm0-3H6V9h12v2zm0-3H6V6h12v2z"/><path d="M0 0h24v24H0z" fill="none"/></svg>'
+      ? '<svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path fill="#0072DD" d="M15.5 1h-8C6.12 1 5 2.12 5 3.5v17C5 21.88 6.12 23 7.5 23h8c1.38 0 2.5-1.12 2.5-2.5v-17C18 2.12 16.88 1 15.5 1zm-4 21c-.83 0-1.5-.67-1.5-1.5s.67-1.5 1.5-1.5 1.5.67 1.5 1.5-.67 1.5-1.5 1.5zm4.5-4H7V4h9v14z"/><path d="M0 0h24v24H0z" fill="none"/></svg>'
+      : '<svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path fill="#0072DD" d="M20 2H4c-1.1 0-1.99.9-1.99 2L2 22l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-2 12H6v-2h12v2zm0-3H6V9h12v2zm0-3H6V6h12v2z"/><path d="M0 0h24v24H0z" fill="none"/></svg>'
   );
 
   return 'data:image/svg+xml,' + dataUrl;
@@ -170,6 +170,10 @@ angular.module('stormpath.mfa', ['stormpath.CONFIG', 'stormpath.utils'])
   };
 }])
 
+.factory('FactorViewModel', function() {
+  return FactorViewModel;
+})
+
 .controller('spMultifactorChallengeFormCtrl', ['StormpathMultifactorAuthenticator', '$scope', function(StormpathMultifactorAuthenticator, $scope) {
   $scope.submit = function submit() {
     $scope.posting = true;
@@ -182,6 +186,17 @@ angular.module('stormpath.mfa', ['stormpath.CONFIG', 'stormpath.utils'])
   $scope.submit = function submit() {
     $scope.posting = true;
     return StormpathMultifactorAuthenticator.enroll($scope.factor, $scope.newFactor);
+  };
+}])
+
+.controller('spMultifactorSelectFormCtrl', ['StormpathMultifactorAuthenticator', 'FactorViewModel', '$scope', function(StormpathMultifactorAuthenticator, FactorViewModel, $scope) {
+  $scope.factorViewModels = $scope.factors.map(function(factor) {
+    return new FactorViewModel(factor);
+  });
+
+  $scope.select = function select(factor) {
+    $scope.posting = true;
+    return StormpathMultifactorAuthenticator.selectFactor(factor);
   };
 }])
 
@@ -217,6 +232,19 @@ angular.module('stormpath.mfa', ['stormpath.CONFIG', 'stormpath.utils'])
       posting: '='
     },
     controller: 'spMultifactorEnrollmentFormCtrl'
+  };
+})
+
+.directive('spMultifactorSelectForm', function() {
+  return {
+    templateUrl: function(tElement, tAttrs) {
+      return tAttrs.templateUrl || 'spMultifactorSelectForm.tpl.html';
+    },
+    scope: {
+      factors: '=',
+      posting: '='
+    },
+    controller: 'spMultifactorSelectFormCtrl'
   };
 })
 

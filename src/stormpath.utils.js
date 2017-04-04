614adf906ad0fd17a02ff973baa9fcf4c0ce2eab
David Campbell <dcampbell@chepri.com>
2017-04-04 15:23:11 -0400

    Issue/214 - Fix IE11 error where  checks link.host, which on https servers returns domain_name:443

--- src/stormpath.utils.js
+++ src/stormpath.utils.js
@@ -13,7 +13,7 @@ angular.module('stormpath.utils', ['stormpath.CONFIG'])
     var link = $window.document.createElement('a');
     link.href = url;
 
-    return $window.location.host === link.host;
+    return $window.location.host === link.hostname;
   };
 }])
 .constant('$spHeaders', {
